e08d6bf083854f6f5eaacd66cbd001387ada9adb
s.nakamura <shotenba@gmail.com>
2024-04-21 14:15:58 +0900

    微修正

--- src/main/java/springchatapp/demo/controller/TaskController.java
+++ src/main/java/springchatapp/demo/controller/TaskController.java
@@ -10,6 +10,7 @@ import springchatapp.demo.model.resource.TaskResourceFactory;
 import springchatapp.demo.service.TaskService;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,13 +19,19 @@ import java.util.Optional;
 public class TaskController {
     private final TaskService taskService;
 
+    /**
+     * ユーザのタスク一覧を取得する。
+     *
+     * @param uid ユーザーID
+     * @return タスク追加後のメッセージを含む {@link ResponseEntity}
+     */
     @GetMapping("/todo/tasklist/{uid}")
-    private ResponseEntity<?> getTaskList(@PathVariable String uid) {
+    public ResponseEntity<?> getTaskList(@PathVariable String uid) {
         Optional<List<TaskEntity>> optionalTaskEntities = taskService.getTaskList(uid);
 
         // タスクがない場合は早期リターン
         if (optionalTaskEntities.isEmpty()) {
-            return ResponseEntity.ok().body(null);
+            return ResponseEntity.ok().body(Collections.emptyList());
         }
 
         List<TaskResource> taskListEntityList = new ArrayList<>();
@@ -37,7 +44,7 @@ public class TaskController {
 
     // TODO: 実装の肉付けをしたい
     @PostMapping("/todo/tasklist/{uid}")
-    private ResponseEntity<?> addTask(@PathVariable int uid, @RequestBody TaskRequestResource taskRequestResource) {
+    public ResponseEntity<?> addTask(@PathVariable String uid, @RequestBody TaskRequestResource taskRequestResource) {
 
         return ResponseEntity.ok("User Info");
     }
